61ee5de3ce51d6e89545c0e9966c1cd1e8724b77
alinakassym <alinakassym@gmail.com>
2025-05-25 23:07:23 +0500

    feat: update HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,23 @@
 import {Link} from "react-router-dom"
+import DiamondIcon from "@mui/icons-material/Diamond"
+import LocalFireDepartmentIcon from "@mui/icons-material/LocalFireDepartment"
+import "./HomePage.css"
 function HomePage() {
   return (
-    <div>
-      <h2>Home</h2>
-      <Link to="/profile">Перейти в профиль</Link>
+    <div className="home-page">
+      <div className="home-page-header">
+        <div className="home-page-header-icon-item" style={{color: "#3A94E7"}}>
+          <DiamondIcon />
+          <span>3</span>
+        </div>
+        <div className="home-page-header-icon-item" style={{color: "#F3AE29"}}>
+          <LocalFireDepartmentIcon />
+          <span>3</span>
+        </div>
+      </div>
+      <div className="home-page-content">
+        <div>Content</div>
+      </div>
     </div>
   )
 }
